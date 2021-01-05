a752f73fd420c26ee3ef3be21287531bfbd51389
Alan Gonzalez <aladinglz@gmail.com>
2021-01-05 14:08:38 -0600

    Complete dashboard layout with dummy data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
+  
+  branchesNumber: number = 1;
+  commitsNumber: number = 3;
+  creationDate: string = '2021-01-05T06:26:21Z';
+  lastCommit = {
+    "date": '2021-01-05T06:26:21Z',
+    "message": 'Create navbar and basic layout\n\nAdded a bootstrap navbar for navigation and basic routing layout. A Dashboard component with no real content was created as well, it will be loaded by default',
+    "filesNumber": 6,
+    "author": 'Alan Gonzalez'
+  }
   constructor() { }
 
   ngOnInit(): void {
