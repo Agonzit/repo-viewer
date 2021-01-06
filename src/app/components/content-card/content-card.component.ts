abdc35a3fb86151758282f827790203dfa5642e8
Alan Gonzalez <aladinglz@gmail.com>
2021-01-06 00:55:04 -0600

    Create ContentCard Component
    Create a presentational component to render commit data as cards

---
+++ src/app/components/content-card/content-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Commit } from 'src/app/models/commit.model';
+
+@Component({
+  selector: 'content-card',
+  templateUrl: './content-card.component.html',
+  styleUrls: ['./content-card.component.scss']
+})
+export class ContentCardComponent implements OnInit {
+
+ /* @Input() public title: string | '' = '';
+  @Input() public subtitle: string | ''= '';
+  @Input() public body: string | '' = '';
+*/
+  @Input() public data: Commit;
+
+  constructor() { this.data = new Commit({});}
+
+  ngOnInit(): void {
+  }
+
+}
